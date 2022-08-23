a7d1200cdc76b0e21493f23f8a26f77060e700d3
Wajeeha <wajeehaakbar944@gmail.com>
2022-08-23 11:42:01 +0500

    footerandcontact

---
+++ src/Components/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+const Contact = () => {
+    return (
+        <>
+        <div className="fst-italic fw-bold fs-4">
+             OH, Hello</div>
+       
+        </>
+    );
+};
+export default Contact;
---
+++ src/Components/images/fontawsome/iconLibrary.js
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+    faCamera,
+    faClipboard
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+    faInstagram,
+    faFacebook,
+    faTwitter,
+    faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+    faInstagram,
+    faFacebook,
+    faTwitter,
+    faLinkedin
+);
