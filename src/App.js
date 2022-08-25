858087d3092de2dde3ba213fa66542692f218b85
Wajeeha <wajeehaakbar944@gmail.com>
2022-08-25 15:14:15 +0500

    contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import IndexCSS from "./Components/IndexCSS.css";
 import AboutUS from "./Components/AboutUs";
+import Contact from "./Components/contact";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="" element={<Navbar />} />
         <Route path="/AboutUS" element={<AboutUS />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </>
   );
--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,16 +1,73 @@
 import React from "react";
+import contact from "./images/contact.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera, faClipboard } from "@fortawesome/free-solid-svg-icons";
 
-
-
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
-    return (
-        <>
-        <div className="fst-italic fw-bold fs-4">
-             OH, Hello
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-8">
+          <div className="flex mb-4 mt-4">
+            <span className="main-h fst-italic fs-5 text-muted ">OH, Hello</span>
+            <span className="date fst-italic fs-5 text-muted float-end">08/08/22</span>
+          </div>
+          {/*image  */}
+          <div className="img">
+            <img className="contact" src={contact} />
+          </div>
         </div>
-       
-        </>
-    );
+        {/* col2 */}
+        <div className="col-md-4 mt-8">
+          {/* welcome */}
+          <div className="welcome list-group-flush">
+            <h1 className="fst-italic fs-3 text-black ">Welcome</h1>
+            <p className="text-muted mt-4 mb-4">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus tempora tenetur explicabo architecto aperiam vitae
+              voluptatibus placeat doloribus quisquam eveniet!
+            </p>
+            {/* icon */}
+
+            <div className="icon pt-12 mt-4 mb-4">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className=" mx-3 h-9 w-6 text-black"
+              />
+              {/* facebook */}
+              <FontAwesomeIcon icon={faFacebook} className=" mx-3 h-9 w-6" />
+              {/* twitter */}
+              <FontAwesomeIcon icon={faTwitter} className=" mx-3 h-9 w-6" />
+              {/* linkdin */}
+              <FontAwesomeIcon icon={faLinkedin} className=" mx-3 h-9 w-6 " />
+            </div>
+
+            {/* categories */}
+            <div className="cat">
+              <h1 className="fst-italic fs-3 text-black">Categories</h1>
+              <ul className="list">
+                <li>Art</li>
+                <li>Inspiration</li>
+                <li>Life</li>
+              </ul>
+              {/* website */}
+              <a className="search" href="/">
+                Search A Website
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* welcome col */}
+    </>
+  );
 };
 export default Contact;
